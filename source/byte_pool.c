248cfe45f0205bb1f880ab20c8af8db3e1e645e2
Andrew Wade <andrew@beartooth.com>
2019-02-10 13:48:31 -0800

    added more byte pool tests

--- source/byte_pool.c
+++ source/byte_pool.c
@@ -13,20 +13,35 @@ struct byte_header_t {
 };
 
 byte_header_t *get_header_from_memory(void *memory);
+
 bool byte_block_is_free(byte_header_t *block);
+
 bool byte_block_is_valid(byte_header_t *block);
+
 size_t byte_block_get_size(byte_header_t **block);
+
+bool byte_block_needs_merge(byte_header_t *block) {
+    return byte_block_is_valid(block)
+           && byte_block_is_valid(block->next)
+           && byte_block_is_free(block->next);
+}
+
 void byte_block_merge_next(byte_pool_t *pool, byte_header_t *block);
-int byte_block_needs_split(byte_header_t **block, size_t size);
+
+bool byte_block_needs_split(byte_header_t **block, size_t size);
+
 void *byte_block_allocate(byte_pool_t *pool, byte_header_t **block, size_t size);
-void byte_block_split(byte_header_t **block, size_t size);
-void byte_block_free(byte_pool_t *pool, byte_header_t **block);
+
+void byte_block_split(byte_pool_t *pool, byte_header_t **block, size_t size);
+
+void byte_block_free(byte_pool_t *pool, byte_header_t *block);
+
 byte_header_t *byte_block_get_next(byte_header_t *);
 
 void byte_pool_init(byte_pool_t *pool, void *memory, size_t size) {
     byte_header_t *header;
 
-    if(pool != NULL && memory != NULL && size > BYTE_BLOCK_MIN) {
+    if (pool != NULL && memory != NULL && size > BYTE_BLOCK_MIN) {
         pool->start     = memory;
         pool->search    = memory;
         pool->end       = memory + size - sizeof(byte_header_t);
@@ -48,10 +63,11 @@ void byte_pool_defragment(byte_pool_t *pool) {
         block = pool->start;
         while (byte_block_is_valid(block)) {
             if (byte_block_is_free(block)) {
-                next = byte_block_get_next(block);
-                if (byte_block_is_valid(next) && byte_block_is_free(next)) {
+                if (byte_block_needs_merge(block)) {
                     byte_block_merge_next(pool, block);
                 } else {
+                    // skip next because it is not free
+                    next = byte_block_get_next(block);
                     block = byte_block_get_next(next);
                 }
             } else {
@@ -97,18 +113,20 @@ void byte_release(void *memory) {
 
     if (!byte_block_is_free(block)) {
         byte_pool_t *pool = block->owner;
-        if(byte_pool_is_valid(pool)) {
-            byte_block_free(pool, &block);
+        if (byte_pool_is_valid(pool)) {
+            byte_block_free(pool, block);
         }
     }
 }
 
 size_t byte_size(void *memory) {
-    byte_header_t *block = get_header_from_memory(memory);
-    if (!byte_block_is_free(block)) {
-        byte_pool_t *pool = block->owner;
-        if(byte_pool_is_valid(pool)) {
-            return byte_block_get_size(&block);
+    if(memory != NULL) {
+        byte_header_t *block = get_header_from_memory(memory);
+        if (!byte_block_is_free(block)) {
+            byte_pool_t *pool = block->owner;
+            if (byte_pool_is_valid(pool)) {
+                return byte_block_get_size(&block);
+            }
         }
     }
     return 0;
@@ -116,8 +134,13 @@ size_t byte_size(void *memory) {
 
 int byte_pool_is_valid(byte_pool_t *pool) {
     return (pool != NULL)
+           && (pool->start != NULL)
+           && (pool->end != NULL)
+           && (pool->search != NULL || pool->capacity == 0)
            && (pool->start < pool->end)
-           && (pool->capacity <= (pool->end - pool->start));
+           && (pool->capacity < (pool->end - pool->start))
+           && (pool->fragments > 0)
+           && (pool->fragments < (pool->end - pool->start) / sizeof(byte_header_t));
 }
 
 byte_header_t *get_header_from_memory(void *memory) {
@@ -134,61 +157,80 @@ bool byte_block_is_valid(byte_header_t *block) {
 
 size_t byte_block_get_size(byte_header_t **block) {
     size_t size = 0;
-    if (byte_block_is_valid(*block)) {
+    if (block != NULL && byte_block_is_valid(*block)) {
         size = ((void *) (*block)->next - (void *) (*block + 1));
     }
     return size;
 }
 
 void byte_block_merge_next(byte_pool_t *pool, byte_header_t *block) {
-    byte_header_t *next = block->next;
-    block->next = next->next;
-    pool->fragments--;
-    if (pool->search == next) {
-        pool->search = block;
+    if (pool != NULL && block != NULL) {
+        byte_header_t *next = block->next;
+        block->next = next->next;
+        pool->fragments--;
+        pool->capacity += sizeof(byte_header_t);
+        if (pool->search == next) {
+            pool->search = block;
+
+        }
     }
 }
 
-int byte_block_needs_split(byte_header_t **block, size_t size) {
-    return (byte_block_get_size(block) - size > BYTE_BLOCK_MIN);
+bool byte_block_needs_split(byte_header_t **block, size_t size) {
+    size_t block_size = byte_block_get_size(block);
+    if (block == NULL || size == 0 || block_size == 0) {
+        return false;
+    } else {
+        return (block_size - size >= BYTE_BLOCK_MIN);
+    }
 }
 
-void byte_block_split(byte_header_t **block, size_t size) {
+void byte_block_split(byte_pool_t *pool, byte_header_t **block, size_t size) {
     byte_header_t *head, *split, *tail;
-
-    if (byte_block_needs_split(block, size)) {
-        head  = *block;
-        tail  = head->next;
-        split = (void *) (head + 1) + size;
-        head->next  = split;
-        split->next = tail;
+    if (pool != NULL && block != NULL && size > 0) {
+        if (byte_block_needs_split(block, size)) {
+            head  = *block;
+            tail  = head->next;
+            split = (void *) (head + 1) + size;
+            head->next  = split;
+            split->next = tail;
+            pool->fragments++;
+            pool->capacity -= sizeof(byte_header_t);
+        }
     }
 }
 
 void *byte_block_allocate(byte_pool_t *pool, byte_header_t **block, size_t size) {
     void *return_ptr = NULL;
 
-    if (byte_block_needs_split(block, size)) {
-        byte_block_split(block, size);
-        pool->fragments++;
-    }
+    if (pool != NULL && block != NULL && size > 0) {
+        if (byte_block_needs_split(block, size)) {
+            byte_block_split(pool, block, size);
+        }
 
-    if (byte_block_get_size(block) >= size) {
-        (*block)->owner = pool;
-        return_ptr = *block + 1;
-        pool->capacity -= (byte_block_get_size(block) + sizeof(void *));
-        if((*block) == pool->search) {
-            pool->search = (*block)->next;
+        if (byte_block_get_size(block) >= size) {
+            (*block)->owner = pool;
+            return_ptr = *block + 1;
+            pool->capacity -= byte_block_get_size(block);
+            if ((*block) == pool->search) {
+                pool->search = (*block)->next;
+            }
         }
     }
 
     return return_ptr;
 }
 
-void byte_block_free(byte_pool_t *pool, byte_header_t **block) {
-    (*block)->owner = NULL;
-    pool->capacity += byte_block_get_size(block) + sizeof(byte_header_t);
+void byte_block_free(byte_pool_t *pool, byte_header_t *block) {
+    if(byte_pool_is_valid(pool) && byte_block_is_valid(block)) {
+        block->owner = NULL;
+        pool->capacity += byte_block_get_size(&block);
+        if(byte_block_needs_merge(block)) {
+            byte_block_merge_next(pool, block);
+        }
+    }
 }
+
 byte_header_t *byte_block_get_next(byte_header_t *block) {
     byte_header_t *next = NULL;
 
--- test/test_byte_pool.cpp
+++ test/test_byte_pool.cpp
@@ -5,29 +5,32 @@
 #include "byte_pool.h"
 
 extern "C" {
-    typedef struct byte_header_t byte_header_t;
+typedef struct byte_header_t byte_header_t;
 
-    struct byte_header_t {
-        byte_header_t *next;
-        byte_pool_t   *owner;
-    };
+struct byte_header_t {
+    byte_header_t *next;
+    byte_pool_t   *owner;
+};
 
-    byte_header_t *get_header_from_memory(void *memory);
-    bool byte_block_is_free(byte_header_t *block);
-    bool byte_block_is_valid(byte_header_t *block);
-    size_t byte_block_get_size(byte_header_t **block);
-    void byte_block_merge_next(byte_pool_t *pool, byte_header_t *block)
-    int byte_block_needs_split(byte_header_t **block, size_t size);
-    void *byte_block_allocate(byte_pool_t *pool, byte_header_t **block, size_t size);
-    void byte_block_split(byte_header_t **block, size_t size);
-    void byte_block_free(byte_pool_t *pool, byte_header_t **block);
-    byte_header_t *byte_block_get_next(byte_header_t *);
+byte_header_t *get_header_from_memory(void *memory);
+bool byte_block_is_free(byte_header_t *block);
+bool byte_block_is_valid(byte_header_t *block);
+size_t byte_block_get_size(byte_header_t **block);
+void byte_block_merge_next(byte_pool_t *pool, byte_header_t *block);
+bool byte_block_needs_split(byte_header_t **block, size_t size);
+void *byte_block_allocate(byte_pool_t *pool, byte_header_t **block, size_t size);
+void byte_block_split(byte_pool_t *pool, byte_header_t **block, size_t size);
+void byte_block_free(byte_pool_t *pool, byte_header_t *block);
+byte_header_t *byte_block_get_next(byte_header_t *);
 };
 
 class BytePoolTestFixture : public testing::Test {
 public:
-    BytePoolTestFixture() {
-        pool = empty;
+    BytePoolTestFixture() : pool((byte_pool_t) {NULL, NULL, NULL, 0, 0}),
+                            empty((byte_pool_t) {NULL, NULL, NULL, 0, 0}),
+                            buffer(),
+                            size(256) {
+
     }
 
     void SetUp() {
@@ -53,16 +56,208 @@ public:
     }
 
     byte_pool_t       pool;
-    const byte_pool_t empty = {
-        .start = NULL,
-        .search = NULL,
-        .end = NULL,
-        .capacity = 0,
-        .fragments = 0
-    };
+    const byte_pool_t empty;
     uint8_t           buffer[256];
-    const size_t      size  = 256;
+    const size_t      size;
 };
+
+TEST_F(BytePoolTestFixture, private_get_header_from_memory) {
+    byte_header_t header;
+    EXPECT_EQ(get_header_from_memory(&header + 1), &header);
+}
+
+TEST_F(BytePoolTestFixture, private_byte_block_is_free) {
+    byte_header_t block = {.owner = NULL};
+    EXPECT_FALSE(byte_block_is_free(NULL));
+    EXPECT_TRUE(byte_block_is_free(&block));
+    block.owner = &pool;
+    EXPECT_FALSE(byte_block_is_free(&block));
+}
+
+TEST_F(BytePoolTestFixture, private_byte_block_is_valid) {
+    byte_header_t block = {.owner = NULL, .next = NULL};
+    byte_header_t next;
+    EXPECT_FALSE(byte_block_is_valid(NULL));
+    EXPECT_FALSE(byte_block_is_valid(&block));
+    block.next = &next;
+    EXPECT_TRUE(byte_block_is_valid(&block));
+}
+
+TEST_F(BytePoolTestFixture, private_byte_block_get_size) {
+    byte_header_t block      = {.owner = &pool, .next = NULL};
+    byte_header_t *block_ptr = &block;
+    byte_header_t *next      = block_ptr + 2;
+    EXPECT_EQ(byte_block_get_size(NULL), 0);
+    EXPECT_EQ(byte_block_get_size(&block_ptr), 0);
+    block.next = next;
+    EXPECT_EQ(byte_block_get_size(&block_ptr), sizeof(block));
+}
+
+TEST_F(BytePoolTestFixture, private_byte_block_merge_next) {
+    byte_header_t block[6] = {
+        {.owner = &pool, .next = &block[2]},
+        {.owner = NULL, .next = NULL},
+        {.owner = NULL, .next = &block[4]},
+        {.owner = NULL, .next = NULL},
+        {.owner = NULL, .next = &block[5]},
+        {.owner = NULL, .next = NULL}};
+    byte_header_t *first   = block;
+    byte_header_t *second  = &block[2];
+    byte_header_t *third   = &block[4];
+    byte_header_t *last    = &block[5];
+    pool.capacity  = sizeof(byte_header_t) * 3;
+    pool.search    = second;
+    pool.fragments = 3;
+    pool.start     = block;
+    pool.end       = block + 6;
+
+    byte_pool_t pool_before_merge = pool;
+
+    byte_block_merge_next(NULL, first);
+    EXPECT_EQ(memcmp(&pool_before_merge, &pool, sizeof(pool)), 0);
+
+    byte_block_merge_next(&pool, NULL);
+    EXPECT_EQ(memcmp(&pool_before_merge, &pool, sizeof(pool)), 0);
+
+    byte_block_merge_next(&pool, first);
+    EXPECT_EQ(pool.fragments, pool_before_merge.fragments - 1);
+    EXPECT_EQ(pool.search, first);
+    EXPECT_EQ(pool.capacity, pool_before_merge.capacity + sizeof(byte_header_t));
+    EXPECT_EQ(first->next, third);
+}
+
+TEST_F(BytePoolTestFixture, private_byte_block_needs_split) {
+    byte_header_t block      = {NULL, NULL};
+    byte_header_t *block_ptr = &block;
+
+    // null block should be false
+    EXPECT_FALSE(byte_block_needs_split(NULL, 16));
+
+    // zero size should be false
+    EXPECT_FALSE(byte_block_needs_split(&block_ptr, 0));
+
+    // bad block should be false
+    EXPECT_FALSE(byte_block_needs_split(&block_ptr, 16));
+
+    // good block should be true if remaining size >= BYTE_BLOCK_MIN
+    block.next = block_ptr + 3;
+    EXPECT_TRUE(byte_block_needs_split(&block_ptr, sizeof(block)));
+
+    // good block should be false if remaining size <BYTE_BLOCK_MIN
+    block.next = block_ptr + 2;
+    EXPECT_FALSE(byte_block_needs_split(&block_ptr, sizeof(block)));
+}
+
+TEST_F(BytePoolTestFixture, private_byte_block_split) {
+    byte_header_t block[6] = {
+        {.owner = &pool, .next = &block[4]},
+        {.owner = NULL, .next = NULL},
+        {.owner = NULL, .next = NULL},
+        {.owner = NULL, .next = NULL},
+        {.owner = NULL, .next = &block[5]},
+        {.owner = NULL, .next = NULL}};
+    byte_header_t *first   = block;
+    byte_header_t *second  = &block[2];
+    byte_header_t *third   = &block[4];
+    byte_header_t *last    = &block[5];
+    pool.capacity  = sizeof(byte_header_t) * 3;
+    pool.search    = second;
+    pool.fragments = 3;
+    pool.start     = block;
+    pool.end       = block + 6;
+
+    byte_pool_t pool_before_split = pool;
+
+    // should ignore null pool
+    byte_block_split(NULL, &first, sizeof(byte_header_t));
+    EXPECT_EQ(memcmp(&pool_before_split, &pool, sizeof(pool)), 0);
+
+    // should ignore null block
+    byte_block_split(&pool, NULL, sizeof(byte_header_t));
+    EXPECT_EQ(memcmp(&pool_before_split, &pool, sizeof(pool)), 0);
+
+    // should ignore zero size
+    byte_block_split(&pool, &first, 0);
+    EXPECT_EQ(memcmp(&pool_before_split, &pool, sizeof(pool)), 0);
+
+    // should split to second pointer
+    byte_block_split(&pool, &first, sizeof(byte_header_t));
+    EXPECT_EQ(first->next, second);
+    EXPECT_EQ(second->next, third);
+    EXPECT_EQ(pool.fragments, pool_before_split.fragments + 1);
+    EXPECT_EQ(pool.capacity, pool_before_split.capacity - sizeof(byte_header_t));
+    EXPECT_EQ(pool.search, pool_before_split.search);
+}
+
+TEST_F(BytePoolTestFixture, private_byte_block_allocate) {
+    PoolInit();
+    byte_pool_t   pool_before_allocate  = pool;
+    byte_header_t block_before_allocate = *(byte_header_t *) pool.start;
+    byte_header_t *block                = (byte_header_t *) pool.start;
+
+    // should ignore null pool
+    byte_block_allocate(NULL, &block, sizeof(byte_header_t));
+    EXPECT_EQ(memcmp(&pool_before_allocate, &pool, sizeof(pool)), 0);
+    EXPECT_EQ(memcmp(&block_before_allocate, pool.start, sizeof(block_before_allocate)), 0);
+
+    // should ignore null block
+    byte_block_allocate(&pool, NULL, sizeof(byte_header_t));
+    EXPECT_EQ(memcmp(&pool_before_allocate, &pool, sizeof(pool)), 0);
+    EXPECT_EQ(memcmp(&block_before_allocate, pool.start, sizeof(block_before_allocate)), 0);
+
+    // should ignore size zero
+    byte_block_allocate(&pool, &block, 0);
+    EXPECT_EQ(memcmp(&pool_before_allocate, &pool, sizeof(pool)), 0);
+    EXPECT_EQ(memcmp(&block_before_allocate, pool.start, sizeof(block_before_allocate)), 0);
+
+    // should reduce capacity and set owner and move search pointer
+    byte_block_allocate(&pool, &block, sizeof(byte_header_t));
+    EXPECT_NE(memcmp(&block_before_allocate, pool.start, sizeof(block_before_allocate)), 0);
+    EXPECT_EQ(pool.capacity, pool_before_allocate.capacity - sizeof(byte_header_t) * 2);
+    EXPECT_EQ(block->owner, &pool);
+    EXPECT_EQ(pool.search, &buffer[sizeof(byte_header_t) * 2]);
+}
+
+TEST_F(BytePoolTestFixture, byte_block_free) {
+    PoolInit();
+    void          *data             = byte_allocate(&pool, 16);
+    byte_pool_t   pool_before_free  = pool;
+    byte_header_t *block            = (byte_header_t *) pool.start;
+    byte_header_t block_before_free = *block;
+
+    // should ignore NULL pool
+    byte_block_free(NULL, block);
+    EXPECT_EQ(memcmp(&pool_before_free, &pool, sizeof(pool)), 0);
+    EXPECT_EQ(memcmp(block, &block_before_free, sizeof(block_before_free)), 0);
+
+    // should ignore NULL block
+    byte_block_free(&pool, NULL);
+    EXPECT_EQ(memcmp(&pool_before_free, &pool, sizeof(pool)), 0);
+    EXPECT_EQ(memcmp(block, &block_before_free, sizeof(block_before_free)), 0);
+
+    // should release owner and increase capacity
+    byte_block_free(&pool, block);
+    EXPECT_EQ(block->owner, nullptr);
+    EXPECT_EQ(pool.capacity, pool_before_free.capacity + 16 + sizeof(byte_header_t));
+}
+
+TEST_F(BytePoolTestFixture, byte_block_get_next) {
+    byte_header_t invalid_block = {NULL, NULL};
+    byte_header_t *block_ptr;
+    EXPECT_EQ(byte_block_get_next(NULL), nullptr);
+    EXPECT_EQ(byte_block_get_next(&invalid_block), nullptr);
+
+    // return null if block is the last block in pool list
+    PoolInit();
+    EXPECT_EQ(byte_block_get_next((byte_header_t *) pool.start), nullptr);
+
+    // return next if next block is valid
+    byte_allocate(&pool, 16);
+    block_ptr = (byte_header_t*)pool.start;
+    EXPECT_EQ(byte_block_get_next(block_ptr), ((byte_header_t *) pool.start)->next);
+    // return null when block is end of pool
+    EXPECT_EQ(byte_block_get_next(block_ptr->next), nullptr);
+}
 
 TEST_F(BytePoolTestFixture, init_ignores_bad_inputs) {
     byte_pool_init(NULL, buffer, size);
@@ -100,4 +295,81 @@ TEST_F(BytePoolTestFixture, allocate_moves_search_pointer) {
     void *expected_search_pointer_after_allocate = (uint8_t *) pool.search + 32 + sizeof(byte_header_t);
     byte_allocate(&pool, 32);
     EXPECT_EQ(expected_search_pointer_after_allocate, pool.search);
+}
+
+TEST_F(BytePoolTestFixture, allocate_returns_new_pointer_until_empty) {
+    PoolInit();
+
+    void   *allocations[size];
+    size_t expected_capacity_after_allocate;
+
+    for (int i = 0; pool.capacity > 16; i++) {
+        expected_capacity_after_allocate = pool.capacity - 16 - sizeof(byte_header_t);
+        allocations[i] = byte_allocate(&pool, 16);
+        EXPECT_EQ(expected_capacity_after_allocate, pool.capacity);
+        ASSERT_NE(allocations[i], nullptr);
+        for (int j     = 0; j < i; j++) {
+            EXPECT_NE(allocations[i], allocations[j]);
+        }
+    }
+    EXPECT_EQ(byte_allocate(&pool, 16), nullptr);
+}
+
+TEST_F(BytePoolTestFixture, is_valid_catches_bad_pool_config) {
+    // start cannot be null
+    PoolInit();
+    pool.start = NULL;
+    EXPECT_FALSE(byte_pool_is_valid(&pool));
+
+    // end cannot be null
+    PoolInit();
+    pool.end = NULL;
+    EXPECT_FALSE(byte_pool_is_valid(&pool));
+
+    // end cannot equal start
+    PoolInit();
+    pool.end = pool.start;
+    EXPECT_FALSE(byte_pool_is_valid(&pool));
+
+    // end cannot be less than start
+    PoolInit();
+    pool.end = (int *) pool.start - 1;
+
+    // search cannot be null if capacity is not zero
+    PoolInit();
+    pool.search = NULL;
+    EXPECT_FALSE(byte_pool_is_valid(&pool));
+
+    // search must be null if capacity is zero
+    PoolInit();
+    pool.search   = NULL;
+    pool.capacity = 0;
+    EXPECT_TRUE(byte_pool_is_valid(&pool));
+
+    // capacity cannot be greater or equal to end - start
+    PoolInit();
+    pool.capacity = (char *) pool.end - (char *) pool.start;
+    EXPECT_FALSE(byte_pool_is_valid(&pool));
+
+    // fragments cannot be zero
+    PoolInit();
+    pool.fragments = 0;
+    EXPECT_FALSE(byte_pool_is_valid(&pool));
+}
+
+TEST_F(BytePoolTestFixture, byte_size_returns_zero_for_invalid_inputs) {
+    PoolInit();
+
+    // null should return 0
+    EXPECT_EQ(byte_size(NULL), 0);
+
+    // free blocks should return zero
+    EXPECT_EQ(byte_size(((byte_header_t*)pool.start)+1), 0);
+}
+
+TEST_F(BytePoolTestFixture, byte_size_returns_correct_size_for_allocated_memory) {
+    PoolInit();
+    // non-free blocks should return correct size
+    byte_allocate(&pool, 32);
+    EXPECT_EQ(byte_size(((byte_header_t*)pool.start)+1), 32);
 }
